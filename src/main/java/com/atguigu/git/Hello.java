ec9e78438d6c59d2809704a495937b2db1c59aca
wenhao <710073113@qq.com>
2018-10-24 15:31:46 +0800

    create project GitTest by wenhao

---
+++ src/main/java/com/atguigu/git/Hello.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author wenhao
+ * @date 2018/10/24
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
---
+++ src/test/java/com/atguigu/git/HelloTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+/**
+ * @author wenhao
+ * @date 2018/10/24
+ */
+public class HelloTest {
+
+}
