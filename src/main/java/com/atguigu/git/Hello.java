ab98cfec7f2afecf56ca93c5b7ab5d1e2c120c9c
wenhaotest02 <44387368+wenhaotest02@users.noreply.github.com>
2018-10-24 17:25:51 +0800

    edit by github
    edit by github

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -9,6 +9,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("helllo github!!!!");
+        System.out.println("hello java");
     }
 
 }
